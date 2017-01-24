04455777ce67d160c84bb2e7da21ab078e4f02d4
puhabg <puhabg@live.com>
2017-01-24 16:52:57 +0200

    Adds logic to return 304 Not Modified for images and caches them on the client

--- MvcEssentials/Web/MvcEssentials.Web/Controllers/FileController.cs
+++ MvcEssentials/Web/MvcEssentials.Web/Controllers/FileController.cs
@@ -1,6 +1,11 @@
 ﻿namespace MvcEssentials.Web.Controllers
 {
+    using System;
+    using System.Globalization;
+    using System.Web;
     using System.Web.Mvc;
+    using Data.Models;
+
     using Services.Data;
 
     public class FileController : Controller
@@ -16,7 +21,43 @@
         public ActionResult Index(int id)
         {
             var fileToRetrieve = this.fileService.GetById(id);
+            if (this.CheckStatus304(fileToRetrieve))
+            {
+                return this.Content(string.Empty);
+            }
+
             return this.File(fileToRetrieve.Content, fileToRetrieve.ContentType);
+        }
+
+        private bool CheckStatus304(Image image)
+        {
+            // http://weblogs.asp.net/jeff/304-your-images-from-a-database
+            DateTime lastModified;
+            if (image.ModifiedOn == null)
+            {
+                lastModified = image.CreatedOn;
+            }
+            else
+            {
+                lastModified = (DateTime)image.ModifiedOn;
+            }
+
+            if (!string.IsNullOrEmpty(this.Request.Headers["If-Modified-Since"]))
+            {
+                CultureInfo provider = CultureInfo.InvariantCulture;
+                var lastMod = DateTime.ParseExact(this.Request.Headers["If-Modified-Since"], "r", provider).ToLocalTime();
+                if (lastMod == lastModified.AddMilliseconds(-lastModified.Millisecond))
+                {
+                    this.Response.StatusCode = 304;
+                    this.Response.StatusDescription = "Not Modified";
+                    return true;
+                }
+            }
+
+            this.Response.Cache.SetCacheability(HttpCacheability.Public);
+            this.Response.Cache.SetLastModified(lastModified);
+
+            return false;
         }
     }
 }
